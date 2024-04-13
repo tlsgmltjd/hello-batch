7cde00f65c560ae05e7248459b7ea0b348b2281b
신희성 <vvwv3092@gmail.com>
2024-04-13 18:27:36 +0900

    Add validator, compositeJobParametersValidator

--- src/main/java/com/example/hellobatch/job/ValidatedParam/ValidatedParamJobConfig.java
+++ src/main/java/com/example/hellobatch/job/ValidatedParam/ValidatedParamJobConfig.java
@@ -1,5 +1,6 @@
 package com.example.hellobatch.job.ValidatedParam;
 
+import com.example.hellobatch.job.ValidatedParam.Validator.FileParamValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
@@ -7,6 +8,7 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.job.CompositeJobParametersValidator;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
@@ -14,10 +16,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Arrays;
+
 
 /*
  * desc: 파일 이름 파리미터 전달, 그리고 검증
- * run: --spring.batch.job.names=validatedParamJob
+ * run: --spring.batch.job.names=validatedParamJob -fileName=hello.csv <- program arguments
  * */
 @Configuration
 @RequiredArgsConstructor
@@ -30,6 +34,7 @@ public class ValidatedParamJobConfig {
     public Job validatedParamJob(Step validatedParamStep) {
         return jobBuilderFactory.get("validatedParamJob")
                 .incrementer(new RunIdIncrementer())
+                .validator(multipleValidator())
                 .start(validatedParamStep)
                 .build();
     }
@@ -50,6 +55,13 @@ public class ValidatedParamJobConfig {
             System.out.println("validation param Spring-Batch");
             return RepeatStatus.FINISHED;
         };
+    }
+
+    // 여러개의 validator를 등록할 수 있음
+    private CompositeJobParametersValidator multipleValidator() {
+        CompositeJobParametersValidator validator = new CompositeJobParametersValidator();
+        validator.setValidators(Arrays.asList(new FileParamValidator()));
+        return validator;
     }
 
 }
